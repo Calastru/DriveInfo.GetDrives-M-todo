3970e15d5d7af500f070145cf4bb60f498de0f90
Marcos <86388553+Calastru@users.noreply.github.com>
2022-10-01 20:17:55 -0300

    Update Program.cs

--- DriveInfo.GetDrives Método/Program.cs
+++ DriveInfo.GetDrives Método/Program.cs
@@ -32,28 +32,3 @@ class Test
         Console.ReadKey();
     }
 }
-/*
-Este código produz uma saída semelhante à seguinte:
-
-Unidade A:\
-  Tipo de unidade: removível
-Unidade C:\
-  Tipo de acionamento: Fixo
-  Rótulo de volume:
-  Sistema de arquivos: FAT32
-  Espaço disponível para o usuário atual: 4770430976 bytes
-  Espaço total disponível: 4770430976 bytes
-  Tamanho total da unidade: 10731683840 bytes
-Unidade D:\
-  Tipo de acionamento: Fixo
-  Rótulo de volume:
-  Sistema de arquivos: NTFS
-  Espaço disponível para o usuário atual: 15114977280 bytes
-  Espaço total disponível: 15114977280 bytes
-  Tamanho total da unidade: 25958948864 bytes
-Unidade E:\
-  Tipo de unidade: CDRom
-
-A saída real deste código varia de acordo com a máquina e as permissões
-concedido ao usuário que o executa.
-*/
